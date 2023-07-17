aa8189dd0ed290225fb73fda88a4777fd9a0bb2b
victorhazbun <victorhazbun87@gmail.com>
2023-07-16 20:01:24 -0500

    current user data

--- src/routes/login/username/+page.svelte
+++ src/routes/login/username/+page.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
   import AuthCheck from "$lib/components/AuthCheck.svelte";
-  import { db, user } from "$lib/firebase";
+  import { db, user, userData } from "$lib/firebase";
   import { doc, setDoc, getDocs, collection, query, where, getDoc } from "firebase/firestore";
 
   let username = "";
   let loading = false;
   let isAvailable = false;
-  let currentUser: any = null; // Variable to store the current user's data
   let errorMessage = ""; // Error message to display to the user
 
   const re = /^(?=[a-zA-Z0-9._]{3,16}$)(?!.*[_.]{2})[^_.].*[^_.]$/;
@@ -16,17 +15,6 @@
   $: isTaken = isValid && !isAvailable && !loading;
 
   let debounceTimer: NodeJS.Timeout;
-
-  async function getCurrentUserData() {
-    try {
-      const userDocRef = doc(db, "users", $user!.uid);
-      const userDocSnapshot = await getDoc(userDocRef);
-      currentUser = userDocSnapshot.exists() ? userDocSnapshot.data() : null;
-    } catch (error) {
-      console.error("Error occurred while retrieving user data:", error);
-      errorMessage = "An error occurred. Please try again.";
-    }
-  }
 
   async function checkAvailability() {
     isAvailable = false;
@@ -44,11 +32,7 @@
 
       try {
         const querySnapshot = await getDocs(q);
-
         isAvailable = querySnapshot.empty;
-
-        await getCurrentUserData(); // Retrieve the current user's data
-
         loading = false;
       } catch (error) {
         console.error("Error occurred while checking availability:", error);
@@ -86,47 +70,51 @@
 </script>
 
 <AuthCheck>
-  <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
-    <input
-      type="text"
-      placeholder="Username"
-      class="input w-full"
-      bind:value={username}
-      on:input={checkAvailability}
-      class:input-error={!isValid && isTouched}
-      class:input-warning={isTaken}
-      class:input-success={isAvailable && isValid && !loading}
-    />
-    <div class="my-4 min-h-16 px-8 w-full">
-      {#if loading}
-        <p class="text-secondary">Checking availability of @{username}...</p>
-      {/if}
+  {#if $userData?.username}
+    <p class="text-lg">
+      Your username is <span class="text-success font-bold"
+        >@{$userData.username}</span
+      >
+    </p>
+    <p class="text-sm">(Usernames cannot be changed)</p>
+    <a class="btn btn-primary" href="/login/photo">Upload Profile Image</a>
+  {:else}
+    <form class="w-2/5" on:submit|preventDefault={confirmUsername}>
+      <input
+        type="text"
+        placeholder="Username"
+        class="input w-full"
+        bind:value={username}
+        on:input={checkAvailability}
+        class:input-error={!isValid && isTouched}
+        class:input-warning={isTaken}
+        class:input-success={isAvailable && isValid && !loading}
+      />
+      <div class="my-4 min-h-16 px-8 w-full">
+        {#if loading}
+          <p class="text-secondary">Checking availability of @{username}...</p>
+        {/if}
 
-      {#if !isValid && isTouched}
-        <p class="text-error text-sm">
-          Must be 3-16 characters long, alphanumeric only.
-        </p>
-      {/if}
-
-      {#if errorMessage}
-        <p class="text-error text-sm">{errorMessage}</p>
-      {/if}
-
-      {#if isValid && !isAvailable && !loading}
-        {#if username === currentUser?.username}
-          <p class="text-warning text-sm">
-            @{username} is your current username.
+        {#if !isValid && isTouched}
+          <p class="text-error text-sm">
+            Must be 3-16 characters long, alphanumeric only.
           </p>
-        {:else}
+        {/if}
+
+        {#if errorMessage}
+          <p class="text-error text-sm">{errorMessage}</p>
+        {/if}
+
+        {#if isValid && !isAvailable && !loading}
           <p class="text-warning text-sm">
             @{username} is not available.
           </p>
         {/if}
-      {/if}
 
-      {#if isAvailable}
-        <button class="btn btn-success" disabled={!!errorMessage}>Confirm username @{username}</button>
-      {/if}
-    </div>
-  </form>
+        {#if isAvailable}
+          <button class="btn btn-success" disabled={!!errorMessage}>Confirm username @{username}</button>
+        {/if}
+      </div>
+    </form>
+  {/if}
 </AuthCheck>
